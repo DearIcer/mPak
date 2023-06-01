16e6ba0bb73793d1e3036f49ffa19d486099dacf
inite <main@inite.cn>
2023-06-01 10:36:42 +0800

    update bat file

--- mPak/ThirdParty/CookContent.bat
+++ mPak/ThirdParty/CookContent.bat
@@ -1,14 +1,15 @@
 @echo off
 
-set EDITOR_PATH=%1        REM 设置编辑器路径
-set PROJECT_PATH=%2       REM 设置项目路径
-set TARGET_PLATFORM=%3    REM 设置目标平台
+rem copyright @ https://inite.cn
 
-rem 构建命令行字符串
+set EDITOR_PATH=%1        REM Set the editor path
+set PROJECT_PATH=%2       REM Set the project path
+set TARGET_PLATFORM=%3    REM Set the target platform
+
+rem Build command line string
 set CommandLine=%EDITOR_PATH% %PROJECT_PATH% -run=cook -targetplatform=%TARGET_PLATFORM%  -unversioned -CrashForUAT -NoLogTimes -UTF8Output
 
-rem 打印命令行以进行调试
+rem Print the command line for debugging
 echo %CommandLine%
 
-rem 执行命令行
-%CommandLine%
+rem Execute the command line
